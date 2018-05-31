7bb6c8c4e71eeb541302c7e877a0f45f3f22056e
Florent Nuttens <fnuttens@gmail.com>
2018-05-31 19:30:56 +0200

    Built base application

--- server/server.js
+++ server/server.js
@@ -1,51 +1,12 @@
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
+var app = express();
 
-// var newTodo = new Todo({
-//   text: 'Edit this video',
-//   completed: false
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
-});
-
-var newUser = new User({
-  email: 'fnuttens@gmail.com'
-});
-
-newUser.save().then((doc) => {
-  console.log('Saved user', doc);
-}, (e) => {
-  console.log('Unable to save user', e);
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
