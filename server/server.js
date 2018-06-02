e4987cb29cfc5121db6c68c3c7cbf6099ceed53c
Florent Nuttens <fnuttens@gmail.com>
2018-06-02 15:28:27 +0200

    Added POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -20,8 +20,8 @@ app.post('/todos', (req, res) => {
     text: req.body.text
   });
 
-  todo.save().then((doc) => {
-    res.send(doc);
+  todo.save().then((todo) => {
+    res.send(todo);
   }, (e) => {
     res.status(400).send(e);
   });
@@ -94,6 +94,19 @@ app.patch('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch((e) => {
     res.status(400).send({});
+  });
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
   });
 });
 
